d5dc8b39fe5305977bc9683baa415656e31b5586
naveenmech <vas.naveenkumar99@gmail.com>
2023-07-14 15:36:32 +0530

    uncomment in usestate

--- app/pin-builder/page.js
+++ app/pin-builder/page.js
@@ -1,10 +1,10 @@
 "use client"
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { useSession } from 'next-auth/react'
-import React, { useState } from 'react'
+import  { useState } from 'react'
 import app from "@/utils/firebaseConfig";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
-import Image from "next/image";
+// import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 
